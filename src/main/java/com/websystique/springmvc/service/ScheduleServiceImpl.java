e0667b81dc380fccb90e36712b7dc577ba648fd8
Pratama Rizky <pratama.rizky.tif413@polban.ac.id>
2016-05-08 06:16:06 +0700

    update Service Schedule

--- src/main/java/com/websystique/springmvc/service/ScheduleServiceImpl.java
+++ src/main/java/com/websystique/springmvc/service/ScheduleServiceImpl.java
@@ -2,13 +2,20 @@ package com.websystique.springmvc.service;
 
 import java.util.Date;
 
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.websystique.springmvc.dao.ScheduleDao;
 
 @Service("ScheduleService")
+@Transactional
 public class ScheduleServiceImpl implements ScheduleService{
+	
+	@Autowired
 	private ScheduleDao dao;
+	
 	public int roomStatus(String facilityId, Date startDate, Date endDate){
 		return dao.roomStatus(facilityId, startDate, endDate);
 	}
