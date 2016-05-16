d0e4c8e1e5361ece72585cafdeec11c7c86198a5
Ryan <ryanhaidypapudi@gmail.com>
2016-05-16 12:08:40 +0700

    Fix AppConfig

--- src/main/java/com/websystique/springmvc/configuration/AppConfig.java
+++ src/main/java/com/websystique/springmvc/configuration/AppConfig.java
@@ -25,7 +25,7 @@ public class AppConfig extends WebMvcConfigurerAdapter{
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/static/**").addResourceLocations("/static/");
+		registry.addResourceHandler("/app/**").addResourceLocations("/app/");
 	}
 
 }
