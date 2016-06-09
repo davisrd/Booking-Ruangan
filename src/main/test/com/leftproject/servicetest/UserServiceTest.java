2d231580af63dc6566e2f50c0cfccd939d834aa4
APRIL <APRIL@APRIL-PC>
2016-06-09 20:55:49 +0700

    fuuuhhhh

---
+++ src/main/test/com/leftproject/servicetest/UserServiceTest.java
@@ -0,0 +1,15 @@
+package com.leftproject.servicetest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
+
+public class UserServiceTest extends AbstractJUnit4SpringContextTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
