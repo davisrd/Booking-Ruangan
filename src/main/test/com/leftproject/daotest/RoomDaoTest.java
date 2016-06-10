82800500cf7c7e911dcb04e1447d0b39054d63d2
davisrd <davis_vario@yahoo.com>
2016-06-10 08:04:07 +0700

    RoomDaoTest

--- src/main/test/com/leftproject/daotest/RoomDaoTest.java
+++ src/main/test/com/leftproject/daotest/RoomDaoTest.java
@@ -1,4 +1,6 @@
 package com.leftproject.daotest;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.List;
 import static org.junit.Assert.*;
 
@@ -6,43 +8,57 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
-
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+
 import com.leftproject.model.*;
 import com.websystique.springmvc.dao.*;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(classes = TestConfiguration.class, loader=AnnotationConfigContextLoader.class)
 public abstract class RoomDaoTest extends AbstractJUnit4SpringContextTests{
 	
 	@Autowired
 	private RoomDao roomDao;
 	
 	@Test
-	public void getRoomListTest()
+	public void getRoomTest()
 	{
-		Room room = roomDao.getRoom("RS001");
-		assertEquals("Gedung Kuliah Bersama",room.getRoomName());
-		
-		
-		
+		Room room = null;
+		try{
+			room = roomDao.getRoom("RS001");
+		} catch(Exception ex) {
+			System.out.println("Fail");
+		}
+		assertEquals(room.getRoomCode(),"RS001");
 	}
 	
 	@Test
 	public void getReservationRoomTest()
 	{
-		
+		Room room = null;
+		try{
+			room = roomDao.getRoom("RG003");
+		} catch(Exception ex) {
+			System.out.println("Fail");
+		}
+		assertEquals(room.getRoomStatus(),"P");
 	}
 	
 	@Test
 	public void getRentRoomByCategoryTest()
 	{
-		
+		Room room = null;
+		try{
+			room = roomDao.getRoom("RG001");
+		} catch(Exception ex) {
+			System.out.println("Fail");
+		}
+		assertEquals(room.getRoomStatus(),"S");
 	}
 	
-	@Test
-	public void getRoomTest()
-	{
-		
-	}
 	
 	
 }
