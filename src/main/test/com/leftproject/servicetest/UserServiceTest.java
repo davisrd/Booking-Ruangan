2aab6207587e0bd3840c61a0265726688e0cda4a
APRIL <APRIL@10.10.67.55>
2016-06-10 08:39:49 +0700

    update

--- src/main/test/com/leftproject/servicetest/UserServiceTest.java
+++ src/main/test/com/leftproject/servicetest/UserServiceTest.java
@@ -1,15 +1,31 @@
 package com.leftproject.servicetest;
-
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes=TestConfigurationService.class, loader=AnnotationConfigContextLoader.class)
 
 public class UserServiceTest extends AbstractJUnit4SpringContextTests {
-
+	
+	@Autowired
+	private UserService userService;
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testFindById() {
+		User user = userService.findById("UEX000001");
+		assertEquals("UEX000001", user.getUserName());
+	}
+
+	private User findById(String id) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
